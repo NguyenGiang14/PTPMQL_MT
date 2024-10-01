00da2b8d5f0770d5e9a4ba7175197e1fac7fcde4
NguyenGiang14 <giangbnbn123@gmail.com>
2024-10-01 15:21:59 +0700

    udEmployee

---
+++ Mvcmovie/Migrations/20241001081233_Create_table_Employee.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mvcmovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create_table_Employee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    StudentID = table.Column<string>(type: "TEXT", nullable: false),
+                    HoTen = table.Column<string>(type: "TEXT", nullable: true),
+                    Address = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.StudentID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
--- Mvcmovie/obj/Debug/net8.0/Mvcmovie.AssemblyInfo.cs
+++ Mvcmovie/obj/Debug/net8.0/Mvcmovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mvcmovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+172c910aa9d8333909db838e01a6ee9e3b78127b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8c2cc4c2a2fd4fb7edecc2262e483b9fac0d3b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mvcmovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mvcmovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
